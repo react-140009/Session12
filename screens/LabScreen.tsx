eaf081b45320033fc9035d547fac3c43021e0c8d
pashaie <pashaie@users.noreply.github.com>
2022-02-22 18:35:02 +0330

    AsyncStorage

--- screens/LabScreen.tsx
+++ screens/LabScreen.tsx
@@ -1,14 +1,37 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button, Text, View, TextInput} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const LabScreen = ({navigation}: any) => {
   const [text, setText] = useState('');
+
+  useEffect(() => {
+    //IIFE
+    (async () => {
+      try {
+        const txt = await AsyncStorage.getItem('LabScreen_text');
+        if (txt) {
+          setText(txt);
+        }
+      } catch (e) {
+        // saving error
+      }
+    })();
+  });
+
+  const changeText = async (txt: string) => {
+    setText(txt);
+    try {
+      await AsyncStorage.setItem('LabScreen_text', txt);
+    } catch (e) {
+      // saving error
+    }
+  };
+
   return (
     <View>
       <Text>لیست آزمایشگاه ها</Text>
-      <TextInput
-        value={text}
-        onChangeText={(txt: string) => setText(txt)}></TextInput>
+      <TextInput value={text} onChangeText={changeText}></TextInput>
       <Text>{text}</Text>
       <Button title="بازگشت" onPress={() => navigation.pop()}></Button>
     </View>
