02509bf1802f474512e4a17be8ac26dc54a6db15
pashaie <pashaie@users.noreply.github.com>
2022-02-22 19:35:58 +0330

    geoLocation

--- screens/AboutUscreen.tsx
+++ screens/AboutUscreen.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Button, Text, View} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 
 export const AboutUsScreen = ({navigation}: any) => {
+  useEffect(() => {
+    Geolocation.watchPosition(
+      info => console.log(info),
+      err => console.log('-->', err),
+    );
+  });
+  const getLocation = () => {
+    Geolocation.getCurrentPosition(info => console.log(info));
+  };
   return (
     <View>
       <Text>About Us</Text>
+      <Button title="get location" onPress={getLocation}></Button>
+
       <Button title="بازگشت" onPress={() => navigation.pop()}></Button>
     </View>
   );
