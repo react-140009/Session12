2e94b082ad0a8e412e92ab016d15eef01dbc02bd
pashaie <pashaie@users.noreply.github.com>
2022-02-22 21:38:18 +0330

    .

--- screens/PurchaseScreen.tsx
+++ screens/PurchaseScreen.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, View, Linking} from 'react-native';
 
 export const PurchaseScreen = ({navigation}: any) => {
   return (
     <View>
       <Text>خرید اشتراک</Text>
+      <Button
+        title="خرید"
+        onPress={() =>
+          Linking.openURL('https://192.168.1.2:44364/PaymentGateway/send/id/10')
+        }></Button>
       <Button title="بازگشت" onPress={() => navigation.pop()}></Button>
     </View>
   );
