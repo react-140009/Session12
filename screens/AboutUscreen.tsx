c2f2e0aac939633c7f684c4aadeedd61e9d944d2
pashaie <pashaie@users.noreply.github.com>
2022-03-10 18:07:41 +0330

    screens/AboutUscreen:  map

--- screens/AboutUscreen.tsx
+++ screens/AboutUscreen.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import {Button, Text, View, StyleSheet} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 import MapView, {UrlTile, Marker, Circle} from 'react-native-maps';
@@ -10,11 +10,23 @@ const styles = StyleSheet.create({
   },
 });
 export const AboutUsScreen = ({navigation}: any) => {
+  const [location, setLocation] = useState<any>();
+  const mapRef = useRef<any>();
   useEffect(() => {
     // Geolocation.watchPosition(
     //   info => console.log(info),
     //   err => console.log('-->', err),
     // );
+
+    Geolocation.getCurrentPosition(info => {
+      setLocation(info.coords);
+      mapRef.current.animateToRegion({
+        latitude: info.coords.latitude,
+        longitude: info.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    });
   });
   const getLocation = () => {
     Geolocation.getCurrentPosition(info => console.log(info));
@@ -23,10 +35,11 @@ export const AboutUsScreen = ({navigation}: any) => {
     <View>
       <Text>About !</Text>
       <MapView
+        ref={mapRef}
         style={styles.map}
         provider={null}
         initialRegion={{
-          latitude: 35.7154139,
+          latitude: 34.7154139,
           longitude: 51.3977737,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
@@ -36,10 +49,7 @@ export const AboutUsScreen = ({navigation}: any) => {
           maximumZ={19}
           urlTemplate="http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker
-          coordinate={{latitude: 35.7154139, longitude: 51.3977737}}
-          title={'My'}
-        />
+        {location && <Marker coordinate={location} title={'My'} />}
         <Circle
           center={{latitude: 35.7154139, longitude: 51.3977737}}
           radius={1000}></Circle>
